bb84acb4bfcc129664fac58bdacb4a86d8358066
Rory McGuire <rjmcguire@gmail.com>
2016-01-05 19:00:00 +0200

    Render output at runtime, or render script at runtime

--- source/app.d
+++ source/app.d
@@ -3,40 +3,19 @@ import std.stdio;
 import jade;
 
 void main(string[] args) {
-	auto filename = "conditionals.jade";
+	auto filename = "plaintext.jade";
 	if (args.length > 1) {
 		filename = args[1];
 	}
-	enum compileTime = render!"conditionals.jade";
-	//pragma(msg, compileTime);
-	//auto writelnSink(string s, int line_number=0, int indent=0) {
-	//	import std.array : replicate;
-	//	import std.string : splitLines;
-	//	if (s == "") return;
-	//	if (indent > 0) {
-	//		foreach (line; s.splitLines) {
-	//			writeln(line_number, "| \t".replicate(indent), line);
-	//		}
-	//	} else {
-	//		writeln(line_number, "| ", s);
-	//	}
-	//}
-	//mixin(compileTime);
-	//writeln("compileTime: %s", compileTime);
+	enum compileTime = render!"plaintext.jade";
 	writeln("templates count: ", compileTime.length);
-	//static if (compileTime.length > 1) {
-	//	foreach (subtemplate; compileTime[1..$]) {
-	//		enum blocks = compileTime[1].findAll("Jade.Block"); // changing this to use the subtemplate causes "subtemplate cannot be read at compile time".
-	//		foreach (block; blocks) {
-	//			writeln(block.matches[0]);
-	//		}
-	//	}
-	//	writeln(compileTime[1].items[1].name);
-	//}
-	compileTime[0].getOutput(compileTime).writeln;
-	//foreach (item; compileTime) {
-	//	writeln(item.toString);
-	//}
-	//writeln("runtime:");
-	//render(stdout, filename);
+
+	enum script = compileTime[0].getOutput(compileTime);
+	pragma(msg, script);
+	writeln("compileTime render:");
+	mixin(script);
+	writeln(buf);
+	writeln("runtime:");
+	auto runtime = render(filename);
+	runtime[0].getOutput(runtime).writeln;
 }
